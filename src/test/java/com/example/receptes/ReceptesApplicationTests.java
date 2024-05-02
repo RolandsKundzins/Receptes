c3f3ca40ce4d9fbd7dade8f29f362a2d11bcd24b
RolandsKundzins <rolis.kungs@inbox.lv>
2024-05-02 10:24:12 +0300

    first commit

---
+++ src/test/java/com/example/receptes/ReceptesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.receptes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
